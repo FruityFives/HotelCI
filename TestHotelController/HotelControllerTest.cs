f2f3c2caff10d4ddd544d19273886f6127c5f7c2
Hudayfa <hhw3103@hotmail.com>
2025-02-27 14:13:25 +0100

    Yes

--- TestHotelController/HotelControllerTest.cs
+++ TestHotelController/HotelControllerTest.cs
@@ -1,6 +1,8 @@
 using HotelWebAPI.Controllers;
 using HotelWebAPI.Data;
+using HotelWebAPI.Models;
 using HotelWebAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TestHotelController
 {
@@ -20,15 +22,19 @@ namespace TestHotelController
         [TestMethod]
         public void Get_WhenCalled_ReturnsOkResult()
         {
+            // Arrange
+        var repository = new HotelRepository();
+        var controller = new HotelsController(repository);
 
-            // Act
+        // Act
+        var result = controller.Get();
 
-
-            // arrange
-
-
-
-            // Assert
+        // Assert
+        var okResult = result as OkObjectResult; // Cast the result to OkObjectResult
+        Assert.IsNotNull(okResult); // Verify the result is not null
+        var returnedHotels = okResult.Value as List<Hotel>; // Cast the value to List<Hotel>
+        Assert.IsNotNull(returnedHotels); // Verify the returned data is not null
+        Assert.AreEqual(4, returnedHotels.Count); // Verify the number of hotels returned
 
         }
 
